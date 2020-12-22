130c831984d3742b9fab39eb893aa2e5d0544dd0
lozanasc <lozanasc@gmail.com>
2020-12-22 22:00:02 +0800

    CSC304 Mini Project Changes: Youtube Downloader API Integration finished

--- CSC304-YoutubeDownloader/YoutubeDownloader.js
+++ CSC304-YoutubeDownloader/YoutubeDownloader.js
@@ -1,12 +1,61 @@
-const URL = document.getElementById('UrlInput');
-const [ About , Submit ] = document.getElementsByTagName('button');
-var isAboutEnabled = false;
+// import Result from './Result';
+
+const URL = document.getElementById('urlInput');
+const AboutInformation = document.querySelector(".groupInformation");
+const [ About , Submit, AboutInformationExit ] = document.getElementsByTagName('button');
+
+const Result = (Title , Description , Thumbnail , isVideo) => {
+    
+    const ResultThumbnail = document.createElement('img');
+    ResultThumbnail.src = Thumbnail;
+
+    const ResultTitle = document.createElement('h1');
+    ResultTitle.innerHTML = Title;
+
+    const ResultDescription = document.createElement('p');
+    ResultDescription.innerHTML = Description;
+
+    const ResultContainer = document.createElement('div');
+    ResultContainer.className = 'result';
+
+
+
+    // isVideo ? doSomethingHere() : doAnotherStuffHere()
+
+    return ResultContainer;
+}
+
+const Download = async(url) => {
+    try {
+        const Data = await fetch(`https://getvideo.p.rapidapi.com/?url=${url}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "SECRET 😜",
+                "x-rapidapi-host": "getvideo.p.rapidapi.com"
+            }
+        });
+        return Data.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 About.addEventListener('click', e => {
     e.preventDefault();
+    AboutInformation.classList.toggle('enable');
 });
 
 Submit.addEventListener('click', e => {
     e.preventDefault();
-    console.log('It works too');
+    Download(encodeURI(URL.value))
+    .then(res => {
+        console.log(res);
+        
+    })
+    .catch(err => console.error(err));
+});
+
+AboutInformationExit.addEventListener('click', e => {
+    e.preventDefault();
+    AboutInformation.className = 'groupInformation disable';
 });
